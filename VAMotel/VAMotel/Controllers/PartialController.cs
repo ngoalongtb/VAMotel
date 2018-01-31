f2a93d11c1473863a393f56ff1937b36c008b71a
DESKTOP-C0L1ALS\ngoal <ngoalongtb001@gmail.com>
2018-01-31 22:21:22 +0700

    create project

---
+++ VAMotel/VAMotel/Controllers/PartialController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VAMotel.Controllers
+{
+    public class PartialController : Controller
+    {
+        // GET: Partial
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Partial
+        public ActionResult SideBarBottom()
+        {
+            return View();
+        }
+        // GET: Partial
+        public ActionResult Footer()
+        {
+            return View();
+        }
+    }
+}
